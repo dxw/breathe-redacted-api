1089c3d6e5838109e121575414a5ca4222c61df1
joseph@dxw.com <joseph@dxw.com>
2023-04-06 16:19:25 +0100

    Reorder file for clarity

--- api.rb
+++ api.rb
@@ -7,12 +7,18 @@ require_relative "./lib/redacted_breathe_client"
 require "sinatra"
 require "sinatra/json"
 
+def valid_key?(key)
+  key.split[1] === ENV.fetch("MIDDLEMAN_API_KEY")
+end
+
 def ensure_key_is_set
   key = ENV.fetch("MIDDLEMAN_API_KEY")
   raise "MIDDLEMAN_API_KEY not set properly" if key.nil? || key.length < 64
 end
 
 ensure_key_is_set
+
+# Routing
 
 before do
   error 401, "API key invalid" unless valid_key?(request.env["HTTP_AUTHORIZATION"])
@@ -32,8 +38,4 @@ end
 
 get "/trainings" do
   json RedactedBreatheClient.trainings(employee_id: params[:employee_id], after: params[:after])
-end
-
-def valid_key?(key)
-  key.split[1] === ENV.fetch("MIDDLEMAN_API_KEY")
 end
