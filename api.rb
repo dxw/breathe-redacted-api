8301f23953fda4e52e2d43b9150f98c90d8f63d9
joseph@dxw.com <joseph@dxw.com>
2023-04-06 16:18:09 +0100

    Add check to make sure key is set and is of sufficient length
    This prevents us from setting an insecure key or inadvertently leaving the key blank

--- api.rb
+++ api.rb
@@ -7,6 +7,13 @@ require_relative "./lib/redacted_breathe_client"
 require "sinatra"
 require "sinatra/json"
 
+def ensure_key_is_set
+  key = ENV.fetch("MIDDLEMAN_API_KEY")
+  raise "MIDDLEMAN_API_KEY not set properly" if key.nil? || key.length < 64
+end
+
+ensure_key_is_set
+
 before do
   error 401, "API key invalid" unless valid_key?(request.env["HTTP_AUTHORIZATION"])
 end
